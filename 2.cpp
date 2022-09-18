a17cf0e46ab04691d0bc301666ff2b4c33a29fe3
ArynAgarwal <96115326+ArynAgarwal@users.noreply.github.com>
2022-09-18 23:43:31 +0530

    Second Question

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	double x;
+	printf("Enter the number: ");
+	scanf("%lf",&x);
+	int n;
+	printf("Enter the digits after the decimal: ");
+	scanf("%d",&n);
+	
+	
+}
