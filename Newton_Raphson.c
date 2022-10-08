00134b2eb996fd0c0c06aa0e04ee46881b915f45
ArynAgarwal <96115326+ArynAgarwal@users.noreply.github.com>
2022-10-08 23:10:37 +0530

    Newton Raphson method!

---
+++ Newton_Raphson.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+float f(float x)
+{	
+    return (float)(3*x-cos(x)-1);
+}
+float df(float x)
+{
+    return (float)(3+sin(x));
+}
+int main()
+{
+    float a=0,b=1,c,res;
+    while(1)
+    {
+        if(f(a)<0&&f(b)>1)
+        break;
+        a=a+1;
+        b=b+1;
+    }
+    c=(a+b)/2;
+    printf("%f\n",c);
+    while(1)
+    {
+        float d=c-f(c)/df(c);
+        printf("%f\n",d);
+        if(fabs(c-d)<0.0001)
+        {
+            res=c;
+            break;
+        }
+        c=d;
+    }
+    printf("\n\n%f",res);
+  return 0;
+}
+
