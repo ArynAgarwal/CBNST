0abb92ec0326fe7037ac89affd5354f1041c002b
ArynAgarwal <96115326+ArynAgarwal@users.noreply.github.com>
2022-12-28 02:23:14 +0530

    Simpsons 3/8 modified method!

---
+++ simpsons3by8.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+
+float findValueAt(float x)
+{
+    return 1/(1+x*x);//exp(pow(x,3));//sin(pow(x,1/2));//1/(1+x*x);
+}
+int main()
+{
+    int n;
+    float i,a,b,sum=0,h;
+    //The initial Position (0) is treated as Even position
+    int position_of_term=1;
+    //Input
+    printf("Enter Value of a and b\n");
+    scanf("%f%f",&a,&b);
+    printf("Enter no. of Intervals\n");
+    scanf("%d",&n);
+
+    h=(b-a)/n;
+    float x[n+1],y[n+1];
+    for(int i=0;i<n+1;i++){
+        x[i]=a + i*h;
+    }
+    for(int i=0;i<n+1;i++){
+        y[i] = findValueAt(x[i]);
+        printf("%0.7f, ",y[i]);
+    }
+    sum = y[0] + y[n];
+    for(int i=1;i<n;i++){
+        if(i%3==0){
+            sum = sum + 2*y[i];
+        }
+        else
+            sum = sum + 3*y[i];
+    }
+    sum = 3*(h * sum)/8;
+    //Print the Output
+    printf("\nValue of The integral  = %0.7f",sum);
+
+}
